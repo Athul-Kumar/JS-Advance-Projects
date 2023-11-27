1d8c1ad8ce6f9bb97f6028eb1c7594050f85ab6e
Athul-Kumar <athulkumar7991@gmail.com>
2023-11-27 15:14:30 +0530

    MEME Generator program is finished

---
+++ MEME Generator/script.js
@@ -0,0 +1,33 @@
+
+const generateBtn = document.querySelector('.meme-generator .generate-meme-btn');
+const memeTitle = document.querySelector('.meme-title');
+const memeImg = document.querySelector('.meme-generator img');
+const memeAuthor = document.querySelector('.meme-generator .meme-author')
+
+
+function getDataResults(title,url,author){
+
+    memeTitle.textContent = title;
+    memeImg.setAttribute('src', url);
+    memeAuthor.textContent = `meme by the author = ${author}`
+
+}
+
+
+
+function generateData(){
+
+    let fetchData = fetch('https://meme-api.com/gimme/wholesomememes');
+    fetchData
+    .then((rawData)=> rawData.json())
+    .then((data)=>{
+        getDataResults(data.title,data.url,data.author)
+    })
+}
+
+generateBtn.addEventListener('click', generateData);
+
+generateData();
+
+
+
